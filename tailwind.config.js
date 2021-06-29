2d9fa4a742762fe7a5309794c0446b5db4f1c71a
pramesywaraj <pramesywaraj@pahamify.com>
2021-06-29 08:31:31 +0700

    remove eslint error

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,11 @@
-const plugin = require('tailwindcss/plugin');
+const plugin = require("tailwindcss/plugin");
 
 function pxToRem(pixel) {
     return `${pixel / 16}rem`;
 }
 
 module.exports = {
-    purge: ['./src/pages/**/*.js', './src/components/**/*.js'],
+    purge: ["./src/pages/**/*.js", "./src/components/**/*.js"],
     darkMode: false, // or 'media' or 'class'
     fontFamily: {
         sans: ["Open Sans", "sans-serif"],
@@ -24,15 +24,15 @@ module.exports = {
             },
         },
         fontSize: {
-            "header1": [pxToRem(40), "58px"],
-            "header2": [pxToRem(32), "48px"],
-            "header3": [pxToRem(28), "40px"],
-            "header4": [pxToRem(24), "32px"],
-            "header5": [pxToRem(20), "28px"],
-            "body": [pxToRem(16), "24px"],
-            "small": [pxToRem(14), "24px"],
-            "xsmall": [pxToRem(12), "20px"],
-            "xxsmall": [pxToRem(10), "16px"],
+            header1: [pxToRem(40), "58px"],
+            header2: [pxToRem(32), "48px"],
+            header3: [pxToRem(28), "40px"],
+            header4: [pxToRem(24), "32px"],
+            header5: [pxToRem(20), "28px"],
+            body: [pxToRem(16), "24px"],
+            small: [pxToRem(14), "24px"],
+            xsmall: [pxToRem(12), "20px"],
+            xxsmall: [pxToRem(10), "16px"],
         },
         fontWeight: {
             bold: 700,
@@ -41,30 +41,30 @@ module.exports = {
             light: 400,
         },
         screens: {
-            "smallPhone": { "max": "374px" },
-            "phone": { "min": "375px", "max": "767px" },
-            "tablet": { "min": "768px", "max": "1023px" },
-            "lgTablet": { "min": "1024px", "max": "1279px" },
-            "desktop": { "min": "1280px", "max": "1919px" },
-            "fullDesktop": "1920px",
+            smallPhone: { max: "374px" },
+            phone: { min: "375px", max: "767px" },
+            tablet: { min: "768px", max: "1023px" },
+            lgTablet: { min: "1024px", max: "1279px" },
+            desktop: { min: "1280px", max: "1919px" },
+            fullDesktop: "1920px",
             "2k": "2048px",
             "4k": "3840px",
-        }
+        },
     },
     variants: {
         extend: {},
     },
     plugins: [
-        plugin(function({ addBase, theme }) {
+        plugin(function ({ addBase, theme }) {
             addBase({
-                'h1': { fontSize: theme('fontSize.header1') },
-                'h2': { fontSize: theme('fontSize.header2') },
-                'h3': { fontSize: theme('fontSize.header3') },
-                'h4': { fontSize: theme('fontSize.header4') },
-                'h5': { fontSize: theme('fontSize.header5') },
-                'p': { fontSize: theme('fontSize.body') },
-                'small': { fontSize: theme('fontSize.xsmall') },
-            })
-        })
+                h1: { fontSize: theme("fontSize.header1") },
+                h2: { fontSize: theme("fontSize.header2") },
+                h3: { fontSize: theme("fontSize.header3") },
+                h4: { fontSize: theme("fontSize.header4") },
+                h5: { fontSize: theme("fontSize.header5") },
+                p: { fontSize: theme("fontSize.body") },
+                small: { fontSize: theme("fontSize.xsmall") },
+            });
+        }),
     ],
 };
