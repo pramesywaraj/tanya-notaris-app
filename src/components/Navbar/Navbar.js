f322a6ee25634b211829f5d62123f17d9ed9d8a9
pramesywaraj <pramesywaraj@pahamify.com>
2021-07-04 22:45:19 +0700

    add isloggedin button condition

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -3,12 +3,14 @@ import Link from "next/link";
 import Image from "next/image";
 
 import NavbarLogo from "assets/tanya-notaris-navbar-logo.svg";
+import IconUser from "assets/icon-user.svg";
 import MenuIcon from "assets/mobile-menu-burger.svg";
 
 import NavbarSideMenu from "components/Navbar/Sidemenu";
+import { Button } from "components/Button";
 
 import "components/Navbar/navbar.module.css";
-import { Button } from "components/Button";
+import colors from "constants/colors";
 
 function NavbarLinks({ style, link, name }) {
     return (
@@ -41,6 +43,8 @@ function NavbarNav({ links }) {
 
 export default function Navbar() {
     const [isShowSideMenu, setIsShowSideMenu] = useState(false);
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
+
     const links = [
         {
             link: "/about",
@@ -59,6 +63,28 @@ export default function Navbar() {
             name: "Artikel Notaris",
         },
     ];
+    const loginBtn = <Button styles={{ padding: "12px 55px", fontSize: "18px" }}>Masuk</Button>;
+    const logoutBtn = (
+        <>
+            <div className="flex flex-row mr-4 justify-between">
+                <p className="pr-4">Ahmad</p>
+                <Image width="30" height="30" src={IconUser} alt="User Account Icon" />
+            </div>
+            <Button
+                onClick={() => { }}
+                styles={{
+                    height: "56px",
+                    width: "168px",
+                    backgroundColor: "transparent",
+                    border: `1px solid ${colors.primary}`,
+                    color: colors.primary,
+                    fontSize: "18px",
+                }}
+            >
+                Keluar
+            </Button>
+        </>
+    );
 
     const handleSideMenu = () => {
         setIsShowSideMenu(!isShowSideMenu);
@@ -79,9 +105,7 @@ export default function Navbar() {
             </a>
             <NavbarNav links={links} />
             <NavbarSideMenu handleSideMenu={handleSideMenu} isShowSideMenu={isShowSideMenu} />
-            <div className="navbar-login-btn">
-                <Button styles={{ padding: "12px 55px", fontSize: "18px" }}>Masuk</Button>
-            </div>
+            <div className="navbar-login-btn">{!isLoggedIn ? logoutBtn : loginBtn}</div>
             <div className="helper-div" />
         </header>
     );
