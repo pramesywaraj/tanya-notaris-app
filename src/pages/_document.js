27bc8b540bf76fd57c15918fce9b1397df39697e
pramesywaraj <pramesywaraj@pahamify.com>
2021-07-10 12:44:54 +0700

    fix document file

---
+++ src/pages/_document.js
@@ -0,0 +1,26 @@
+import Document, { Html, Head, Main, NextScript } from "next/document";
+
+class MyDocument extends Document {
+    render() {
+        return (
+            <Html lang="en">
+                <Head>
+                    <meta name="msapplication-TileColor" content="#000000" />
+                    <meta name="theme-color" content="#000000" />
+                    <link rel="preconnect" href="https://fonts.googleapis.com" />
+                    <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
+                    <link
+                        href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500;600;700&display=swap"
+                        rel="stylesheet"
+                    />
+                </Head>
+                <body className="antialiased text-black bg-white dark:bg-gray-900 dark:text-white">
+                    <Main />
+                    <NextScript />
+                </body>
+            </Html>
+        );
+    }
+}
+
+export default MyDocument;
