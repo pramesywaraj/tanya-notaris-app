77ec2bf81ecae6133a98255e6cd5c146f87bdbae
pramesywaraj <pramesywaraj@pahamify.com>
2021-07-11 14:24:01 +0700

    fix button component

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -1,10 +1,15 @@
 import "components/Button/button.module.css";
 
-export default function Button({ onClick, children, styles, classNames }) {
+export default function Button({ onClick, children, styles, classNames, type }) {
     const classes = classNames ? classNames : "";
 
     return (
-        <button className={`btn btn-normal ${classes}`} onClick={onClick} style={{ ...styles }}>
+        <button
+            className={`btn btn-normal ${classes}`}
+            onClick={onClick}
+            style={{ ...styles }}
+            type={type || "button"}
+        >
             {children}
         </button>
     );
