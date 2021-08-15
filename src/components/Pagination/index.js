7fa662833c7a538872245971156d3a1a5085a3fd
pramesywaraj <pramesywaraj@pahamify.com>
2021-08-15 22:43:25 +0700

    add pagination component

--- src/components/Pagination/index.js
+++ src/components/Pagination/index.js
@@ -1,42 +1,28 @@
+/* eslint-disable jsx-a11y/interactive-supports-focus */
+/* eslint-disable jsx-a11y/click-events-have-key-events */
 import { useState, useEffect } from "react";
 
 import styles from "components/Pagination/pagination.module.css";
 
 export default function Pagination({ data = [], onPageChange }) {
-    const [selectedIndex, setSelectedIndex] = useState(1);
-    const [firstLoad, setFirstLoad] = useState(true);
-
-    useEffect(() => {
-        if (firstLoad) {
-            setFirstLoad(false);
-
-            return;
-        }
-        onPageChange();
-    }, [selectedIndex]);
-
     if (data.length === 0) return null;
 
     const handleSelectPage = (index) => {
-        setSelectedIndex(index);
+        onPageChange(index);
     };
 
     return (
         <div className={styles["pagination-container"]}>
             {data.map((item, index) => {
-                if (index === 0 || index === data.length - 1) return null;
-
-                const itemClasses =
-                    selectedIndex === index
-                        ? `${styles["pagination-item"]} ${styles["pagination-active"]}`
-                        : `${styles["pagination-item"]}`;
+                const itemClasses = item.active
+                    ? `${styles["pagination-item"]} ${styles["pagination-active"]}`
+                    : `${styles["pagination-item"]}`;
 
                 return (
                     <div
-                        onClick={() => handleSelectPage(index)}
+                        onClick={() => handleSelectPage(Number(item.label))}
                         key={`pag-item-${index + 1}`}
                         className={itemClasses}
-                        style={{ marginLeft: index !== 1 ? 12 : 0 }}
                         role="button"
                     >
                         <p>{item.label}</p>
