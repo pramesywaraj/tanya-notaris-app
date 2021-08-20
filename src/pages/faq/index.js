eb3ea4bcfa3227431d4f9637c27cbe7a2258e533
pramesywaraj <pramesywaraj@pahamify.com>
2021-08-20 22:19:37 +0700

    api integration for faq and handle responsivity

--- src/pages/faq/index.js
+++ src/pages/faq/index.js
@@ -1,25 +1,37 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 import style from "styles/faq.module.css";
 
 import useRequest from "hooks/useRequest";
+import usePagination from "hooks/usePagination";
 
 import { Search } from "components/Search";
-import { QuestionItem, FilterItem } from "components/Items";
+import { QuestionItem } from "components/Items";
 import { FilterBottomSheet } from "components/BottomSheet";
 import { CardFilter } from "components/Card";
+import Pagination from "components/Pagination";
+import { SimpleEmptyState } from "components/EmptyState";
 
 export default function FAQPage() {
     const [isShowFilter, setIsShowFilter] = useState(false);
 
     const [filterCategories, setFilterCategories] = useState([]);
-    const [selectedCategory, setSelectedCategory] = useState(false);
+    const [selectedCategory, setSelectedCategory] = useState(null);
+    const [queryParams, setQueryParams] = useState("");
+    const firstLoad = useRef(true);
 
-    const [contents, setIsContents] = useState([]); // temporary
     const {
         data: typesData,
         error: typesError,
         isLoadingData: isLoadingFetchingTypes,
     } = useRequest("v1/types");
+
+    const {
+        items: questions,
+        paginationItems,
+        error,
+        setPage,
+        isLoadingData: isLoadingFetchQuestions,
+    } = usePagination("v1/faqs", queryParams);
 
     useEffect(() => {
         if (typesData) {
@@ -27,6 +39,18 @@ export default function FAQPage() {
             setFilterCategories(data);
         }
     }, [typesData]);
+
+    useEffect(() => {
+        if (firstLoad.current) {
+            firstLoad.current = false;
+
+            return;
+        }
+
+        const tempCategoryQuery = selectedCategory ? `type[]=${selectedCategory}&` : "";
+
+        setQueryParams(tempCategoryQuery);
+    }, [selectedCategory]);
 
     const handleToggleFilter = (stat) => {
         setIsShowFilter(stat);
@@ -42,11 +66,52 @@ export default function FAQPage() {
         return;
     };
 
+    const handleChangePage = (selectedPage) => {
+        window.scrollTo({
+            top: 0,
+            behavior: "smooth",
+        });
+        setPage(selectedPage);
+    };
+
+    const renderContent = () => {
+        return (
+            <div className={style["faq-container-question"]}>
+                {!isLoadingFetchQuestions &&
+                    (questions || []).map((question, index) => (
+                        <QuestionItem
+                            key={`question-${index + 1}`}
+                            content={question}
+                            containerStyle={{ marginTop: index === 0 ? 0 : 16 }}
+                        />
+                    ))}
+
+                {isLoadingFetchQuestions &&
+                    [1, 2, 3, 4, 5, 6].map((_, index) => (
+                        <QuestionItem
+                            key={`question-${index + 1}`}
+                            content={{}}
+                            containerStyle={{ marginTop: index === 0 ? 0 : 16 }}
+                            isLoading
+                        />
+                    ))}
+
+                {paginationItems && questions.length > 0 && paginationItems.length > 0 && (
+                    <div className={style["faq-pagination-container"]}>
+                        <Pagination data={paginationItems} onPageChange={handleChangePage} />
+                    </div>
+                )}
+
+                {!isLoadingFetchQuestions && questions.length === 0 && <SimpleEmptyState />}
+            </div>
+        );
+    };
+
     return (
         <section className={style["faq-section-container"]}>
             <div className="faq-header">
                 <h1 className="faq-title">Frequently Asked Question</h1>
-                <Search classNames="w-full tablet:w-auto" />
+                <Search classNames="w-full mt-4 tablet:w-auto lgTablet:mt-0" />
             </div>
             <div className="faq-body">
                 <div className="faq-filter">
@@ -59,11 +124,7 @@ export default function FAQPage() {
                         isLoading={isLoadingFetchingTypes}
                     />
                 </div>
-                <div className="faq-question">
-                    {contents.map((content, index) => (
-                        <QuestionItem content={content} />
-                    ))}
-                </div>
+                {!error ? renderContent() : <p>Terdapat Kesalahan.</p>}
                 <button className="filter-button" onClick={() => handleToggleFilter(true)}>
                     Filter
                 </button>
