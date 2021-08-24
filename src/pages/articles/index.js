6e5b7930c111adf269691da3fa0c1070b24a99ff
pramesywaraj <pramesywaraj@pahamify.com>
2021-08-24 23:17:45 +0700

    integrate article with api

--- src/pages/articles/index.js
+++ src/pages/articles/index.js
@@ -1,21 +1,52 @@
-import "styles/articles.module.css";
+import { useState } from "react";
+import useRequest from "hooks/useRequest";
 
-import Highlight from "components/Artikel/Highlight";
+import style from "styles/articles.module.css";
+
 import { Search } from "components/Search";
-import NewArticles from "components/Artikel/NewArticles";
-import { PopularArticles } from "components/Artikel";
+import { PopularArticles, HighlightArticle, NewArticles } from "components/Artikel";
 
-export default function index() {
+const articlesEndpoint = "v1/articles";
+
+export default function ArticlesPage() {
+    const {
+        data: articlesHighlighted,
+        isLoadingData: isLoadingHighlightedArticles,
+        error: errorHighlightedArticle,
+    } = useRequest(`${articlesEndpoint}/highlighted?per_page=3`);
+
+    const {
+        data: articlesPopular,
+        isLoadingData: isLoadingPopularArticles,
+        error: errorPopularArticle,
+    } = useRequest(`${articlesEndpoint}/popular?per_page=4`);
+
+    const {
+        data: articlesLatest,
+        isLoadingData: isLoadingLatestArticles,
+        error: errorLatestArticle,
+    } = useRequest(`${articlesEndpoint}?per_page=4`);
 
     return (
-        <>
-            <section className="header-articles">
-                <h1 className="header-title">Artikel Notaris</h1>
-                <Search classNames="w-full tablet:w-auto"/>
-            </section>
-            <Highlight />
-            <NewArticles />
-            <PopularArticles />
-        </>
-    )
+        <section className={style["articles-section-container"]}>
+            <div className={style["articles-header"]}>
+                <h1 className={style["articles-title"]}>Artikel Notaris</h1>
+                <Search classNames="w-full mt-4 tablet:w-auto lgTablet:mt-0" />
+            </div>
+            <div className={style["articles-content-container"]}>
+                <HighlightArticle
+                    data={articlesHighlighted?.data || []}
+                    isLoading={isLoadingHighlightedArticles}
+                />
+                <NewArticles
+                    data={articlesLatest?.data || []}
+                    isLoading={isLoadingLatestArticles}
+                />
+                <PopularArticles
+                    data={articlesPopular?.data || []}
+                    isLoading={isLoadingPopularArticles}
+                />
+            </div>
+        </section>
+    );
 }
