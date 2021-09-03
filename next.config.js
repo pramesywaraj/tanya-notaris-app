9211280533681b719d284ce936dbf1217bb181c7
pramesywaraj <pramesywaraj@pahamify.com>
2021-09-03 21:28:06 +0700

    - add nookies and transpile module dep - add get, remove, create cookies with nookies package

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
-module.exports = {
+const withTM = require("next-transpile-modules")(["react-markdown"]);
+
+module.exports = withTM({
     reactStrictMode: true,
     env: {
         API_BASE_URL: process.env.API_BASE_URL,
     },
-};
+});
