ef518f060fcc32cb8c423ea705e6ba5c2c36d557
pramesywaraj <pramesywaraj@pahamify.com>
2021-09-05 07:39:23 +0700

    add loader to banner

--- src/components/Banner/BannerService.js
+++ src/components/Banner/BannerService.js
@@ -1,9 +1,13 @@
 import style from "components/Banner/banner.module.css";
+import { SkeletonLoader } from "components/Loader";
 
-export default function BannerService({ name }) {
+export default function BannerService({ name, isLoading }) {
     return (
         <div className={`${style["service-banner-padding"]} ${style["service-banner-container"]}`}>
-            <h2 className={`${style["service-banner-title"]}`}>{name}</h2>
+            {!isLoading && <h2 className={`${style["service-banner-title"]}`}>{name}</h2>}
+            {isLoading && (
+                <SkeletonLoader width={200} height={30} color="#db6842" highlightColor="#cc5b35" />
+            )}
         </div>
     );
 }
